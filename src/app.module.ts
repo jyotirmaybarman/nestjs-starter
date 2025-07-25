9ae949b69e2dc3ad463f929e8507827d633a8f00
Jyotirmay Barman <jyotirmay@developerzilla.com>
2025-07-25 15:35:32 +0530

    fix: replace old redis store with keyv store

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MyTypeOrmModule } from './providers/typeorm/my-typeorm.module';
 import { UsersModule } from 'src/modules/users/users.module';
 import { QueueModule } from './providers/queue/queue.module';
 import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-ioredis-yet';
+import { createKeyv } from '@keyv/redis';
 import { env } from './utils/env';
 import { RequestLoggerMiddleware } from './common/middlewares/request-logger.middleware';
 import { JwtModule } from '@nestjs/jwt';
@@ -17,13 +17,12 @@ import { JwksModule } from './providers/jwks/jwks.module';
     CacheModule.registerAsync({
       isGlobal: true,
       useFactory: async () => ({
-        store: await redisStore({
-          host: env.REDIS_HOST,
-          port: env.REDIS_PORT,
-          db: env.REDIS_QUEUE_DB,
+        store: createKeyv({
+          url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`,
           username: env.REDIS_USERNAME,
           password: env.REDIS_PASSWORD,
-        }),
+          database: env.REDIS_CACHE_DB
+        })
       }),
     }),
     JwtModule.register({
